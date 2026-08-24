c072d474d5b423c54336e14f2b16809560a9706c
yu <river0330mysterybook@gmail.com>
2026-08-24 21:53:33 +0900

    トップページの作成

--- diary/views.py
+++ diary/views.py
@@ -1,5 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
 
 
-def index(request):
-    return HttpResponse("日記アプリです")
+class IndexView(View):
+    def get(self, request):
+        return render(request, "diary/index.html")
+    
+
+index = IndexView.as_view()
