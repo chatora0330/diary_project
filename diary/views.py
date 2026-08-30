9edb9182af4b319d60db96621a348bbebfc86f10
yu <river0330mysterybook@gmail.com>
2026-08-30 20:48:37 +0900

    一覧画面の作成

--- diary/views.py
+++ diary/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import redirect, render
 from django.views import View
 
 from .forms import PageForm
+from .models import Page
 
 
 class IndexView(View):
@@ -30,7 +31,13 @@ class PageCreateView(View):
             form.save()
             return redirect("diary:index")
         return render(request, "diary/page_form.html", {"form": form})
-    
+
+class PageListView(View):
+    def get(self, request):
+        page_list = Page.objects.order_by("-page_date")
+        return render(request, "diary/page_list.html", {"page_list": page_list})
+
     
 index = IndexView.as_view()
 page_create = PageCreateView.as_view()
+page_list = PageListView.as_view()
